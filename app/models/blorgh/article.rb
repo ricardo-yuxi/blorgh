5873a619ed150d4ae4dfdf699b5be4b7a0670c63
Ricardo Rojas <ricardo.rojas@Pablos-iMac.local>
2016-08-24 17:01:58 -0500

    Using a Class Provided by the Application

--- app/models/blorgh/article.rb
+++ app/models/blorgh/article.rb
@@ -1,5 +1,14 @@
 module Blorgh
   class Article < ApplicationRecord
+    attr_accessor :author_name
     has_many :comments
+    belongs_to :author, class_name: "User"
+
+    before_validation :set_author
+
+    private
+      def set_author
+        self.author = User.find_or_create_by(name: author_name)
+      end
   end
 end
