919fffad5e5cb14f87423bc7e9fd9e8a96c4a642
Ricardo Rojas <ricardo.rojas@Pablos-iMac.local>
2016-08-24 13:42:29 -0500

    adds comments functionality

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160824163413) do
+ActiveRecord::Schema.define(version: 20160824165045) do
 
   create_table "blorgh_articles", force: :cascade do |t|
     t.string   "title"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "blorgh_comments", force: :cascade do |t|
+    t.integer  "article_id"
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
